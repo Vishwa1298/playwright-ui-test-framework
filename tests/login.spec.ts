aa54a6eec63262b90c49dbf7eecfa80903425466
Vishwa1298 <vishwajithreddy39@gmail.com>
2026-03-27 09:57:27 +0100

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/loginPage';
+import { LoginPage } from '../pages/loginpage';
 import { loginData } from '../test-data/loginData';
 
 test('Valid login test', async ({ page }) => {
